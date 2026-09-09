4618201d0b63835a96972fd4e09dc0a7f61dda37
Trunks <buttermunchinwalrus@gmail.com>
2026-09-09 02:09:23 -0400

    linux compatiblilty for img ref

--- client/src/Components/Home/Navbar.tsx
+++ client/src/Components/Home/Navbar.tsx
@@ -3,9 +3,9 @@ import styles from "./Home.module.css";
 function NavBar() {
     return (
         <nav className={styles["navbar"]}>
-            <img src="src\assets\MD1400_1.jpg" alt="Simple Logo" className={styles["logo"]}/>
+            <img src="src/assets/MD1400_1.jpg" alt="Simple Logo" className={styles["logo"]}/>
             <h1 className={styles["ch2"]}>Stack Crafted</h1>
-            <img src="src\assets\MD1400_1.jpg" alt="Simple Logo" className={styles["logo"]}/>
+            <img src="src/assets/MD1400_1.jpg" alt="Simple Logo" className={styles["logo"]}/>
         </nav>
     )
 }
--- client/src/Components/Home/Websites.tsx
+++ client/src/Components/Home/Websites.tsx
@@ -6,7 +6,7 @@ function Websites() {
 
                 <div className={styles["card"]} onClick={() => {window.open("/IEEE_Club")}}>
                     <div className={styles["flex-row"]}>
-                        <img src="src\assets\IEEE_Student_Club.jpg" alt="UEEE Student club logo" className={styles["logo-website"]}/>
+                        <img src="src/assets/IEEE_Student_Club.jpg" alt="UEEE Student club logo" className={styles["logo-website"]}/>
                         <div className={styles["card2"]}>
                             <h1 className={styles["ch1"]}>IEEE Club</h1>
                         </div>
@@ -15,7 +15,7 @@ function Websites() {
 
                 <div className={styles["card"]} onClick={() => {window.open("https://github.com/ClownKingWalrus?tab=repositories", "_blank")}}>
                     <div className={styles["flex-row"]}>
-                        <img src="src\assets\Octicons-mark-github.svg" alt="Github SVG Logo" className={styles["logo-website"]}/>
+                        <img src="src/assets/Octicons-mark-github.svg" alt="Github SVG Logo" className={styles["logo-website"]}/>
                         <div className={styles["card2"]}>
                             <h1 className={styles["ch1"]}>Reposotory</h1>
                         </div>
@@ -24,7 +24,7 @@ function Websites() {
 
                 <div className={styles["card"]} onClick={() => {window.open("https://clownkingwalrus.github.io/Portfolio/", "_blank")}}>
                     <div className={styles["flex-row"]}>
-                        <img src="src\assets\Portfolio_Tree.jpg" alt="Portfolio Tree Image" className={styles["logo-website"]}/>
+                        <img src="src/assets/Portfolio_Tree.jpg" alt="Portfolio Tree Image" className={styles["logo-website"]}/>
                         <div className={styles["card2"]}>
                             <h1 className={styles["ch1"]}>Portfolio</h1>
                         </div>
